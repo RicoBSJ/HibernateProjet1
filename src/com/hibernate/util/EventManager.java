00726dd766b8376cbab7c7dbf5cd047aa4d96f58
ricobsj <ricobsj@localhost>
2020-02-10 00:18:49 +0100

    First Repo

---
+++ src/com/hibernate/util/EventManager.java
@@ -0,0 +1,35 @@
+package com.hibernate.util;
+
+import java.util.Date;
+
+import org.hibernate.Session;
+
+import com.hibernate.events.Event;
+
+public class EventManager {
+
+	public static void main(String[] args) {
+		EventManager mgr = new EventManager();
+
+		if (args[0].equals("store")) {
+			mgr.createAndStoreEvent("My Event", new Date());
+		}
+
+		HibernateUtil.getSessionFactory().close();
+	}
+
+	private void createAndStoreEvent(String title, Date theDate) {
+
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+
+		session.beginTransaction();
+
+		Event theEvent = new Event();
+		theEvent.setTitle(title);
+		theEvent.setDate(theDate);
+
+		session.save(theEvent);
+
+		session.getTransaction().commit();
+	}
+}
